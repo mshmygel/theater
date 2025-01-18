19e28cbbc677244f13f9ede2ecf09ba7101bcad1
Mike Shmygel <m.shmygel@gmail.com>
2025-01-18 11:00:05 +0200

    added raise_exceptions=True insted of "if" condition

--- theater/views.py
+++ theater/views.py
@@ -124,11 +124,9 @@ class PlayViewSet(
         play = self.get_object()
         serializer = self.get_serializer(play, data=request.data)
 
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     @extend_schema(
         parameters=[
